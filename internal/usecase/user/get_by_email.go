1be4d1c2f67e47f0adf042a331b907d41e020ecd
FabioRocha231 <fabioharoldo1@gmail.com>
2026-02-18 19:49:11 -0300

    feat: aplying DRY in responses output of user

--- internal/usecase/user/get_by_email.go
+++ internal/usecase/user/get_by_email.go
@@ -2,29 +2,13 @@ package usecase
 
 import (
 	"context"
+
 	ports "github.com/FabioRocha231/saas-core/internal/port"
 	"github.com/FabioRocha231/saas-core/internal/port/repository"
-	"time"
 )
 
 type GetUserByEmailInput struct {
 	Email string
-}
-
-type GetUserByEmailOutput struct {
-	ID              string     `json:"id"`
-	Name            string     `json:"name"`
-	Email           string     `json:"email"`
-	Cpf             string     `json:"cpf"`
-	Phone           string     `json:"phone"`
-	Status          string     `json:"status"`
-	Role            string     `json:"role"`
-	CreatedAt       time.Time  `json:"created_at"`
-	UpdatedAt       time.Time  `json:"updated_at"`
-	DeletedAt       *time.Time `json:"deleted_at"`
-	EmailVerifiedAt *time.Time `json:"email_verified_at"`
-	PhoneVerifiedAt *time.Time `json:"phone_verified_at"`
-	LastLoginAt     *time.Time `json:"last_login_at"`
 }
 
 type GetUserByEmailUsecase struct {
@@ -37,26 +21,12 @@ func NewGetUserByEmailUsecase(repo repository.UserRepository, uuid ports.UUIDInt
 	return &GetUserByEmailUsecase{repo: repo, uuid: uuid, context: ctx}
 }
 
-func (u *GetUserByEmailUsecase) Execute(input GetUserByEmailInput) (*GetUserByEmailOutput, error) {
+func (u *GetUserByEmailUsecase) Execute(input GetUserByEmailInput) (*GetUserOutputDTO, error) {
 	user, err := u.repo.GetByMail(u.context, input.Email)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return &GetUserByEmailOutput{
-		ID:              user.ID,
-		Name:            user.Name,
-		Email:           user.Email,
-		Cpf:             user.Cpf,
-		Phone:           user.Phone,
-		Status:          user.Status.String(),
-		Role:            user.Role.String(),
-		CreatedAt:       user.CreatedAt,
-		UpdatedAt:       user.UpdatedAt,
-		DeletedAt:       user.DeletedAt,
-		EmailVerifiedAt: user.EmailVerifiedAt,
-		PhoneVerifiedAt: user.PhoneVerifiedAt,
-		LastLoginAt:     user.LastLoginAt,
-	}, nil
+	return ToGetUserOutputDTO(user), nil
 }
--- internal/usecase/user/get_by_id.go
+++ internal/usecase/user/get_by_id.go
@@ -2,8 +2,8 @@ package usecase
 
 import (
 	"context"
+
 	"github.com/FabioRocha231/saas-core/internal/domain/errx"
-	"time"
 
 	ports "github.com/FabioRocha231/saas-core/internal/port"
 	"github.com/FabioRocha231/saas-core/internal/port/repository"
@@ -19,27 +19,11 @@ type GetUserByIdInput struct {
 	ID string
 }
 
-type GetUserByIdOutput struct {
-	ID              string     `json:"id"`
-	Name            string     `json:"name"`
-	Email           string     `json:"email"`
-	Cpf             string     `json:"cpf"`
-	Phone           string     `json:"phone"`
-	Status          string     `json:"status"`
-	Role            string     `json:"role"`
-	CreatedAt       time.Time  `json:"created_at"`
-	UpdatedAt       time.Time  `json:"updated_at"`
-	DeletedAt       *time.Time `json:"deleted_at"`
-	EmailVerifiedAt *time.Time `json:"email_verified_at"`
-	PhoneVerifiedAt *time.Time `json:"phone_verified_at"`
-	LastLoginAt     *time.Time `json:"last_login_at"`
-}
-
 func NewGetUserByIdUsecase(repo repository.UserRepository, uuid ports.UUIDInterface, ctx context.Context) *GetUserByIdUsecase {
 	return &GetUserByIdUsecase{repo: repo, uuid: uuid, context: ctx}
 }
 
-func (u *GetUserByIdUsecase) Execute(input GetUserByIdInput) (*GetUserByIdOutput, error) {
+func (u *GetUserByIdUsecase) Execute(input GetUserByIdInput) (*GetUserOutputDTO, error) {
 	isUuid := u.uuid.Validate(input.ID)
 
 	if !isUuid {
@@ -51,19 +35,5 @@ func (u *GetUserByIdUsecase) Execute(input GetUserByIdInput) (*GetUserByIdOutput
 		return nil, err
 	}
 
-	return &GetUserByIdOutput{
-		ID:              user.ID,
-		Name:            user.Name,
-		Email:           user.Email,
-		Cpf:             user.Cpf,
-		Phone:           user.Phone,
-		Status:          user.Status.String(),
-		Role:            user.Role.String(),
-		CreatedAt:       user.CreatedAt,
-		UpdatedAt:       user.UpdatedAt,
-		DeletedAt:       user.DeletedAt,
-		EmailVerifiedAt: user.EmailVerifiedAt,
-		PhoneVerifiedAt: user.PhoneVerifiedAt,
-		LastLoginAt:     user.LastLoginAt,
-	}, nil
+	return ToGetUserOutputDTO(user), nil
 }
